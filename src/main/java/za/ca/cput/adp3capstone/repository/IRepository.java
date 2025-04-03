d7f9c494dfd3fae5a5e26adc63b61e5ef730818b
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-04-03 13:55:59 +0200

    Update IRepository

--- src/main/java/za/ca/cput/adp3capstone/repository/IRepository.java
+++ src/main/java/za/ca/cput/adp3capstone/repository/IRepository.java
@@ -1,8 +1,12 @@
 package za.ca.cput.adp3capstone.repository;
-/* Product.java
+/* IRepository.java
 Product model class
 Author: Ethan Le Roux (222622172)
 Date:3 April 2025
 */
-public interface IRepository {
+public interface IRepository<T, Id>{
+    public T get(Id id);
+    public T create(T t);
+    public T update(T t);
+    public boolean delete(T t);
 }
