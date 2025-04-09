2134f061ea63a06a86edcdac8389f32adef838eb
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-04-09 12:50:19 +0200

    Add getAll method

--- src/main/java/za/ca/cput/adp3capstone/repository/IReviewRepository.java
+++ src/main/java/za/ca/cput/adp3capstone/repository/IReviewRepository.java
@@ -1,8 +1,14 @@
 package za.ca.cput.adp3capstone.repository;
+
+import za.ca.cput.adp3capstone.domain.Review;
+
+import java.util.HashMap;
+
 /* Product.java
 Product model class
 Author: Ethan Le Roux (222622172)
 Date:3 April 2025
 */
-public interface IReviewRepository {
+public interface IReviewRepository extends IRepository<Review, Long> {
+    HashMap<Review,Long>  getAll();
 }
